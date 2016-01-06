26b4c5ee6f6abbac6d155fda64ede8421e2bade4
meanmonkey662 <coolkoch8535@gmail.com>
2016-01-06 11:52:09 -0700

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Koch, Kyle on 1/6/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
